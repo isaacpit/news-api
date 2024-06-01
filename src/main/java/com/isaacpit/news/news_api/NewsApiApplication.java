f51ef7c171d30b98501f5b2ea8cb66c147da874c
isaacpitblado <isaacpitblado@tamu.edu>
2024-05-31 21:52:28 -0500

    added src/ from Spring Initializr

---
+++ src/main/java/com/isaacpit/news/news_api/NewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.isaacpit.news.news_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApiApplication.class, args);
+	}
+
+}
