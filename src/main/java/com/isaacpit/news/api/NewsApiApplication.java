e865f159ce36758bb2eb084879235aaae289434b
isaacpitblado <isaacpitblado@tamu.edu>
2024-06-02 20:02:27 -0500

    removed unnecessary configuration enablement bean

--- src/main/java/com/isaacpit/news/api/NewsApiApplication.java
+++ src/main/java/com/isaacpit/news/api/NewsApiApplication.java
@@ -1,10 +1,11 @@
-package com.isaacpit.news.news_api;
+package com.isaacpit.news.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties
+
 @SpringBootApplication
 public class NewsApiApplication {
 
